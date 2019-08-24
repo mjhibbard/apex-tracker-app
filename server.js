055f8c0d431c5ee5bb61e5bce2c4a112e14ee150
Michael <veg.it.up@live.com>
2019-08-24 12:55:24 -0700

    attempt typo repair to deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api/v1/profile', require('./routes/profile'));
 // Handle Production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder to the build file
-  app.use(express.static(__dirname, '/public/'));
+  app.use(express.static(__dirname, './public/'));
 
   // Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
