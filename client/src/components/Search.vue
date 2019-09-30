ac08673d8d9c56e90e7ea2ef97fe6968ae08f169
Michael <veg.it.up@live.com>
2019-09-29 23:09:50 -0700

    add client file in repo

---
+++ client/src/components/Search.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="search">
+    <h1>Track Player Stats:</h1>
+    <form v-on:submit.prevent="onSubmit">
+      <div class="form-group">
+        <label for="platform">Platform</label>
+        <select name="Platform" id="platform" v-model="platform">
+          <option value="psn">Playstation</option>
+          <option value="xbl">Xbox</option>
+          <option value="Origin">Origin</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="gamertag">Gamer Tag</label>
+        <input
+          type="text"
+          name
+          v-model="gamertag"
+          id="gamertag"
+          placeholder="Origin ID, Xbox Live GamerTag, etc"
+        />
+      </div>
+      <div class="form-group">
+        <input type="submit" class="btn" />
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      platform: "psn",
+      gamertag: ""
+    };
+  },
+  beforeCreate() {
+    document.body.className = "body-bg-image";
+  },
+  methods: {
+    onSubmit() {
+      if (!this.gamertag) {
+        this.$toasted.show("Please enter a gamertag", {
+          duration: 3000,
+          icon: "exclamation-circle"
+        });
+      } else {
+        this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
